ef12b9eb6ac5a39340f32f20e2be7c9d4593b7d3
Vlad300 <vladimirzon1111@gmail.com>
2026-04-21 21:53:05 +0000

    Initial project setup with Spring Boot

---
+++ src/main/java/com/forig/fpms/FpmsApplication.java
@@ -0,0 +1,13 @@
+package com.forig.fpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forig/fpms/FpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forig.fpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
