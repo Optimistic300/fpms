5c44c7a178fd3358c21bb82a845c4b72be54df99
Vlad300 <vladimirzon1111@gmail.com>
2026-07-25 14:21:09 +0000

    Skip exisitng migrations

--- docker/start.sh
+++ docker/start.sh
@@ -10,7 +10,7 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-php artisan migrate --force
+php artisan migrate --force 2>/dev/null || true
 
 php-fpm -D
 nginx -g "daemon off;"
